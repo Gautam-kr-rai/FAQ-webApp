b725bf1b4125e137d6cc25779206aa852901c5db
Gautam Kumar <g.k965069@gmail.com>
2025-02-04 05:03:31 +0530

    some more fixes

--- Frontend/src/lib/axios.js
+++ Frontend/src/lib/axios.js
@@ -1,8 +1,10 @@
 import axios from "axios"
 
 const axiosInstance = axios.create({
-  baseURL:import.meta.mode==="development"? "http://localhost:3000/api":"/api",
-  withCredentials:true, // send cookies to the server
+  baseURL: import.meta.env.MODE === "development" 
+  ? "http://localhost:3000/api" 
+  : "/api", // this assumes that in production the API will be served from the same domain
+withCredentials: true, // send cookies to the server // send cookies to the server
 })
 
 export default axiosInstance;
