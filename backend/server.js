0e72b307c6676cb24ee5ba34ea75f9410f48fa6c
Gautam Kumar <g.k965069@gmail.com>
2025-02-04 04:47:56 +0530

    fixes for the Deployment

--- backend/server.js
+++ backend/server.js
@@ -22,10 +22,10 @@ app.use("/api/faq", faqRoutes);
 
 
 if(process.env.NODE_ENV==="production"){
-  app.use(express.static(path.join(__dirname,"/frontend/dist")))
+  app.use(express.static(path.join(__dirname,"/Frontend/dist")))
 
   app.get("*", (req, res) => {
-   res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+   res.sendFile(path.resolve(__dirname, "Frontend", "dist", "index.html"));
  });
 }
 app.listen(PORT, () => {
