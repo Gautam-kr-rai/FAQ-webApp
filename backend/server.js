66151ba3f4c0a979c04da4b557c89c6fcaa8c15b
Gautam Kumar <g.k965069@gmail.com>
2025-02-05 19:48:45 +0530

    cors changes

--- backend/server.js
+++ backend/server.js
@@ -17,9 +17,8 @@ const __dirname= path.resolve()
 
 
 app.use(cors({
-  origin: "http://localhost:5173/", // Allow frontend domain
-  methods: "GET, POST, PUT, DELETE",
-  credentials: true, // Allow cookies if needed
+      origin: [process.env.FRONTEND_URL || "http://localhost:5173"],
+			credentials: true, // Allow cookies if needed
 }));
 
 app.use(express.json({ limit: "10mb" }));
