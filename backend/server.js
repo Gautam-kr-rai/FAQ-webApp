7fa6722bbece313ae1552ca2d4de8db2e0580c36
Gautam Kumar <g.k965069@gmail.com>
2025-02-07 16:55:48 +0530

    cor's fixes

--- backend/server.js
+++ backend/server.js
@@ -15,11 +15,31 @@ const PORT = process.env.PORT || 5000;
 const __dirname= path.resolve()
 
 
+const allowedOrigins = process.env.CORS_ORIGIN?.split(",");
 
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL || "http://localhost:5173",
-    credentials: true, // Allow cookies if needed
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    credentials: true,
+  })
+);
+
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    credentials: true,
   })
 );
 
